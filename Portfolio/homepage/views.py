7a1b5ec15b40315de49e4abeefb20642b7d2ed92
soumaya boumsiss <soumayaboumsiss@gmail.com>
2026-01-28 14:39:33 +0100

    changes the home.html to index.html

--- Portfolio/homepage/views.py
+++ Portfolio/homepage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'homepage/home.html')
+    return render(request, 'homepage/index.html')
